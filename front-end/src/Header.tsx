6a767f77cec5925adf1a673a9cd0e6f8a9386a84
Pekka Kalpio <kalpio.pekka@gmail.com>
2024-05-17 10:23:01 +0300

    changed to bootstrap

---
+++ front-end/src/Header.tsx
@@ -0,0 +1,19 @@
+import "./header.css";
+
+export function Header () {
+
+	return (
+		<div className="header">
+			<div>
+				<h1>Website name</h1>
+			</div>
+			<div>
+				frontpage
+				ajanavaraus
+				user site
+				<button>
+				Log in</button>
+			</div>
+		</div>
+	);
+}
