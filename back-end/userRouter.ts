acb7babeddf567c48bdca69383c361b40ac38ed1
lpaukkala <laura.paukkala@gmail.com>
2024-05-17 10:27:02 +0300

    add changes to dao.ts, userRouter.ts, package.json, package-lock.json

--- back-end/userRouter.ts
+++ back-end/userRouter.ts
@@ -3,11 +3,18 @@ import dao from './dao'
 
 const userRouter = express.Router();
 
-
 userRouter.get("/:id", async (req, res) => {
     const result = await dao.findUser(Number(req.params.id));
     const user = result.rows[0];
     res.send(user);
+  });
+
+  userRouter.post("/:create", async (req, res) => {
+    const user = req.body;
+    console.log(req.body)
+    const result = await dao.createUser(user);
+    const id = { result.rows[0] };
+    res.send(id);
   });
 
 export default userRouter;
