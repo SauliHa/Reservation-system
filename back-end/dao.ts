4dca5d496256137a991ef7be11187437f18b34ea
lpaukkala <laura.paukkala@gmail.com>
2024-05-17 11:02:36 +0300

    modify dao.ts

--- back-end/dao.ts
+++ back-end/dao.ts
@@ -9,4 +9,14 @@ export const findUser = async (id: number) => {
     return result;
   };
 
-export default {findUser};
+  export const createUser = async (product) => {
+    console.log(product)
+    const id = uuidv4();
+    const params = [id, ...Object.values(product)];
+    const query = `INSERT INTO products (id, name, price) VALUES ($1, $2, $3)`
+    console.log(`Inserting a new product ${params[0]}...`);
+    const result = await executeQuery(query, params);
+    console.log(`New product ${id} inserted succesfully.`);
+    return result;
+  };
+
