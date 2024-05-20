b55ded5c474b74468ce66e453fc976c8f33d31ea
Pekka Kalpio <kalpio.pekka@gmail.com>
2024-05-20 12:45:42 +0300

    header & footer new styling + mdboostrap

--- front-end/src/Header.tsx
+++ front-end/src/Header.tsx
@@ -5,16 +5,13 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 export function Header () {
 
 	return (
-		<div className="header">
-			<div className="headerItem">
-				<h3>Super-sport bowling</h3>
-			</div>
-			<div>
-				<Link to="/" className="headerItem">Front page</Link>
-				<Link to="/user" className="headerItem">Userpage</Link>
-				<Link to="/calendar" className="headerItem">Reservation Calendar</Link>
-				<Link to="/login" className="headerItem"><Button variant="dark">Log in</Button></Link>
-			
+		<div className="superheader">
+			<h3 id="headerTitle">Super-sport bowling</h3>	
+			<div className="header">
+				<li><Link to="/" className="headerItem">Front page</Link></li>
+				<li><Link to="/user" className="headerItem">Userpage</Link></li>
+				<li><Link to="/calendar" className="headerItem">Reservation Calendar</Link></li>
+				<Link to="/login"  id="loginButton"><Button variant="dark">Log in</Button></Link>
 			</div>
 		</div>
 	);
