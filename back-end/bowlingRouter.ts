cdd5c31fd22881009ed66eb7bd281e57864e53fe
lpaukkala <laura.paukkala@gmail.com>
2024-05-23 14:40:50 +0300

    some changes to bowling-dao, bowlingrouter

--- back-end/bowlingRouter.ts
+++ back-end/bowlingRouter.ts
@@ -1,25 +1,30 @@
 import express from "express";
-import { findAllLanes, findLane, findDate } from "./bowling-dao";
+import { findAllLanes, findLane, findDate, createLane } from "./bowling-dao";
 
 const bowlingRouter = express.Router();
 
 bowlingRouter.get("/", async (req, res) => {
 	const result = await findAllLanes();
-	const lanes= result.rows[0];
+	const lanes= result.rows;
 	res.send(lanes);
 });
 
 bowlingRouter.get("/:id", async (req, res) => {
 	const result = await findLane(req.params.id);
-	const lane = result.rows[0];
+	const lane = result.rows;
 	res.send(lane);
 });
 
 bowlingRouter.get("/date/:date", async (req, res) => {
 	const result = await findDate(req.params.date);
-	const dateinfo = result.rows[0];
+	const dateinfo = result.rows;
 	res.send(dateinfo);
 });
 
+bowlingRouter.post("/create", async (req, res) => {
+	const {name} = req.body;
+	const result = await createLane(name);
+	res.send(result.rows[0]);
+});
 
 export default bowlingRouter;
