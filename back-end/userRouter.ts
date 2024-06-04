c8113581f47d9a1454b3413a4a0ecd8d7b2cd693
SauliHa <sauli.hanninen@hotmail.fi>
2024-06-04 13:58:53 +0300

    Fixed login being broken in docker

--- back-end/userRouter.ts
+++ back-end/userRouter.ts
@@ -1,68 +1,83 @@
 import express from "express";
 import * as dao from "./dao";
 import jwt, { TokenExpiredError, JsonWebTokenError } from "jsonwebtoken";
-import { validate, loginSchema, createUserSchema, updateUserSchema } from "./validate";
+import {
+	validate,
+	loginSchema,
+	createUserSchema,
+	updateUserSchema,
+} from "./validate";
 import argon2 from "argon2";
 import dotenv from "dotenv";
 import { authenticate } from "./authenticate";
 dotenv.config();
 
 const userRouter = express.Router();
-
-const createToken = (id: string, username: string, email: string, admin: boolean) => {
+const { SECRET } = process.env;
+const createToken = (
+	id: string,
+	username: string,
+	email: string,
+	admin: boolean
+) => {
 	const payload = { id, username, email, admin };
-	const secret = process.env.secret;
+
 	const options = { expiresIn: "48h" };
-	if (secret === undefined) {
+	if (SECRET === undefined) {
 		return;
 	}
-	const token = jwt.sign(payload, secret, options);
+
+	const token = jwt.sign(payload, SECRET, options);
 	return token;
 };
 
 userRouter.get("/:id", authenticate, async (req, res, next) => {
-	try {const result = await dao.findUser(req.params.id);
+	try {
+		const result = await dao.findUser(req.params.id);
 		const user = result.rows[0];
 		res.send(user);
-	}
-	catch (error) {
-		next(error); 
+	} catch (error) {
+		next(error);
 	}
 });
 
 userRouter.get("/", async (req, res, next) => {
-	try {const result = await dao.findAll();
+	try {
+		const result = await dao.findAll();
 		const users = result.rows;
 		res.send(users);
-	}
-	catch (error) {
-		next(error); 
-	}
-});
-
-userRouter.post("/create", validate(createUserSchema), async (req, res, next) => {
-	const { username, password, email, phone_number, address } = req.body;
-	console.log(req.body);
-	
-	const checkedEmail = await dao.checkEmail(email);
-	if (checkedEmail.rows.length > 0) {
-		res.status(401).send("This email is already in use");
-		return;
-	}
-
-	try {
-		const result = await dao.createUser(
-			username,
-			password,
-			email,
-			phone_number,
-			address
-		);
-		res.send(result.rows[0]);
 	} catch (error) {
-		next(error); 
+		next(error);
 	}
 });
+
+userRouter.post(
+	"/create",
+	validate(createUserSchema),
+	async (req, res, next) => {
+		const { username, password, email, phone_number, address } = req.body;
+		console.log(req.body);
+
+		const checkedEmail = await dao.checkEmail(email);
+		if (checkedEmail.rows.length > 0) {
+			res.status(401).send("This email is already in use");
+			return;
+		}
+
+		try {
+			const result = await dao.createUser(
+				username,
+				password,
+				email,
+				phone_number,
+				address
+			);
+			res.send(result.rows[0]);
+		} catch (error) {
+			next(error);
+		}
+	}
+);
 
 userRouter.post("/login", validate(loginSchema), async (req, res, next) => {
 	const { email, password } = req.body;
@@ -89,9 +104,8 @@ userRouter.post("/login", validate(loginSchema), async (req, res, next) => {
 				res.status(401).send("Unauthorized");
 			}
 		}
-	}
-	catch (error) {
-		next(error); 
+	} catch (error) {
+		next(error);
 	}
 });
 
@@ -102,51 +116,60 @@ userRouter.delete("/:id", authenticate, async (req, res, next) => {
 	if (checkId.rowCount === 0) {
 		res.status(404).send("Error: User not found");
 	}
-	
-	try {
-		await dao.deleteUser(userId);	
-		res.status(200).send(
-			`User with id ${userId} deleted successfully.`
-		);
-	} catch (error)  {
-		next(error); 
-	}
-});
-
-userRouter.put("/:id", authenticate, validate(updateUserSchema), async (req, res, next) => {
-	const { username, password, email, phone_number, address, admin } = req.body;
-	const checkedEmail = await dao.checkEmail(email);
-	if (checkedEmail.rows.length > 0) {
-		res.status(401).send("This email is already in use");
-		return;
-	} 
-	const id = req.params.id;
-	console.log(`Request to change user with id ${id}`);
 
 	try {
-		const result = await dao.updateUser(
-			id,
-			username,
-			password,
-			email,
-			phone_number,
-			address,
-			admin
-		);
-		if (result.rowCount === 0) {
-			res.status(404).send("Error: User not found");
-		} else {
-			const result = await dao.findUser(id);
-			const updatedAdmin = result.rows[0].admin;
-			const updatedUsername = result.rows[0].username;
-			const updatedEmail = result.rows[0].email;
-			const token = createToken(id, updatedUsername, updatedEmail, updatedAdmin);
-			res.status(200).send(token);
-		}
+		await dao.deleteUser(userId);
+		res.status(200).send(`User with id ${userId} deleted successfully.`);
 	} catch (error) {
-		next(error); 
+		next(error);
 	}
 });
+
+userRouter.put(
+	"/:id",
+	authenticate,
+	validate(updateUserSchema),
+	async (req, res, next) => {
+		const { username, password, email, phone_number, address, admin } =
+			req.body;
+		const checkedEmail = await dao.checkEmail(email);
+		if (checkedEmail.rows.length > 0) {
+			res.status(401).send("This email is already in use");
+			return;
+		}
+		const id = req.params.id;
+		console.log(`Request to change user with id ${id}`);
+
+		try {
+			const result = await dao.updateUser(
+				id,
+				username,
+				password,
+				email,
+				phone_number,
+				address,
+				admin
+			);
+			if (result.rowCount === 0) {
+				res.status(404).send("Error: User not found");
+			} else {
+				const result = await dao.findUser(id);
+				const updatedAdmin = result.rows[0].admin;
+				const updatedUsername = result.rows[0].username;
+				const updatedEmail = result.rows[0].email;
+				const token = createToken(
+					id,
+					updatedUsername,
+					updatedEmail,
+					updatedAdmin
+				);
+				res.status(200).send(token);
+			}
+		} catch (error) {
+			next(error);
+		}
+	}
+);
 
 userRouter.get("/token/:token", async (req, res) => {
 	const token = req.params.token;
@@ -178,7 +201,7 @@ userRouter.get("/:id/reservations", authenticate, async (req, res, next) => {
 		const reservations = result.rows;
 		res.send(reservations);
 	} catch (error) {
-		next(error); 
+		next(error);
 	}
 });
 
